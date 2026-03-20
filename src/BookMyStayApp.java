b8c619801db95123f30ad68b55ddb9052e462aed
hanif132007 <hanif132007@gmail.com>
2026-03-20 14:53:48 +0530

    update uc2

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,9 +1,25 @@
-
 public class BookMyStayApp {
 
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        System.out.println("Beds: 1");
+        System.out.println("Size: 250 sqft");
+        System.out.println("Price per night: 1500.0");
+        System.out.println("Available: 5\n");
+
+        System.out.println("Double Room:");
+        System.out.println("Beds: 2");
+        System.out.println("Size: 400 sqft");
+        System.out.println("Price per night: 2500.0");
+        System.out.println("Available: 3\n");
+
+        System.out.println("Suite Room:");
+        System.out.println("Beds: 3");
+        System.out.println("Size: 750 sqft");
+        System.out.println("Price per night: 5000.0");
+        System.out.println("Available: 2");
     }
 }
